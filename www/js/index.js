5e3069c8993e305547c250ad76a9ce333eb031e9
Nikhil Prakash <nikhilprakash510@gmail.com>
2024-05-31 02:03:32 +0530

    Added gallery view

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
+    console.log('Device is ready');
     document.getElementById('takePhoto').addEventListener('click', takePhoto);
     loadGallery();
 }
@@ -13,15 +14,19 @@ function takePhoto() {
 }
 
 function onPhotoSuccess(imageURI) {
+    console.log('Photo taken: ' + imageURI);
     window.resolveLocalFileSystemURL(imageURI, function(fileEntry) {
         var galleryFolder = cordova.file.dataDirectory + "gallery/";
-        window.resolveLocalFileSystemURL(galleryFolder, function() {
+        window.resolveLocalFileSystemURL(galleryFolder, function(dirEntry) {
+            console.log('Gallery folder exists');
             savePhoto(fileEntry);
         }, function() {
+            console.log('Gallery folder does not exist, creating it');
             window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dirEntry) {
-                dirEntry.getDirectory("gallery", { create: true }, function() {
+                dirEntry.getDirectory("gallery", { create: true }, function(newDirEntry) {
+                    console.log('Gallery folder created');
                     savePhoto(fileEntry);
-                });
+                }, onPhotoFail);
             });
         });
     }, onPhotoFail);
@@ -30,8 +35,11 @@ function onPhotoSuccess(imageURI) {
 function savePhoto(fileEntry) {
     var galleryFolder = cordova.file.dataDirectory + "gallery/";
     var newFileName = Date.now() + '.jpg';
-    fileEntry.copyTo(galleryFolder, newFileName, function() {
-        loadGallery();
+    window.resolveLocalFileSystemURL(galleryFolder, function(dirEntry) {
+        fileEntry.copyTo(dirEntry, newFileName, function(newFileEntry) {
+            console.log('Photo saved: ' + newFileName);
+            loadGallery();  // Load the gallery to update it with the new photo
+        }, onPhotoFail);
     }, onPhotoFail);
 }
 
@@ -42,10 +50,18 @@ function loadGallery() {
         directoryReader.readEntries(function(entries) {
             var gallery = document.getElementById('gallery');
             gallery.innerHTML = '';
+            if (entries.length === 0) {
+                gallery.innerHTML = '<p>No photos in gallery.</p>';
+            }
             for (var i = 0; i < entries.length; i++) {
                 if (entries[i].isFile) {
                     var img = document.createElement('img');
                     img.src = entries[i].nativeURL;
+                    img.style.width = '100px';
+                    img.style.height = '100px';
+                    img.style.objectFit = 'cover';
+                    img.style.margin = '5px';
+                    img.style.border = '1px solid #ccc';
                     gallery.appendChild(img);
                 }
             }
@@ -55,4 +71,5 @@ function loadGallery() {
 
 function onPhotoFail(message) {
     alert('Failed because: ' + message);
+    console.log('Failed because: ' + message);
 }
